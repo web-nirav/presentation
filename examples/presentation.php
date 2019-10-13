51f1a210352ea46c4b392bd1501ae6d8309a609d
Nirav Prajapati <web.niravprajapati@gmail.com>
2019-10-13 22:33:05 +0530

    presentation completed

---
+++ examples/presentation.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Gradient backgrounds - fullPage.js</title>
+	<meta name="author" content="Alvaro Trigo Lopez" />
+	<meta name="description" content="fullPage full-screen backgrounds." />
+	<meta name="keywords"  content="fullpage,jquery,demo,screen,fullscreen,backgrounds,full-screen" />
+	<meta name="Resource-type" content="Document" />
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../dist/fullpage.css" />
+	<link rel="stylesheet" type="text/css" href="examples.css" />
+	<link rel="stylesheet" type="text/css" href="presentation.css" />
+	<!--[if IE]>
+		<script type="text/javascript">
+			 var console = { log: function() {} };
+		</script>
+	<![endif]-->
+</head>
+<body>
+
+<div id="fullpage">
+
+    <div class="section " id="section0"><h1>Welcome Everyone!</h1></div>
+    
+	<div class="section " id="section0"><h1>Disclaimer</h1></div>
+
+	<div class="section" id="section1">
+	    <div class="slide" id="slide1">
+            <h2>DOD</h2>
+            <h2>Unit Testing</h2>
+            <h2>Student Syndrom</h2>
+            <h2>Prevention is better than cure</h2>
+            <h2>Trust</h2>
+        </div>
+
+        <div class="slide" id="slide2"><h1>DOD?</h1></div>
+
+        <div class="slide" id="slide2">
+            <h1>Definition of Done (DoD)</h1>
+            <h2>DOD is the primary reporting mechanism for team members.</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h1>DoD can includes</h1>
+            <h2>- Feature has been developed</h2>
+            <h2>- Unite Tested</h2>
+            <h2>- Meets all requirements of perticular feature or module</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>The Dod should be same for each team member at different levels based on their roles and responsiblity </h2>
+            <h2>Here we gonna talk about the DoD for developers.</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>Any question about DOD?</h2>
+        </div>
+
+        <div class="slide" id="slide2"><h1>Student Syndrom</h1></div>
+
+        <div class="slide" id="slide2">
+            <h2>- Student syndrome refers to planned procrastination(<small style="color: cyan;">Procrastination is the avoidance of doing a task that needs to be accomplished by a certain deadline. It could be further stated as a habitual or intentional delay of starting or finishing a task despite knowing it might have negative consequences.</small>), when, for example, a student will only start to apply themselves to an assignment at the last possible moment before its deadline. </h2>
+            <br/>
+            <h2>- This eliminates any potential safety margins and puts the person under stress and pressure.</h2>
+        </div>
+        
+        <div class="slide" id="slide2"><h1>Unit Testing</h1></div>
+
+        <div class="slide" id="slide2">
+            <h2>What is unit testing? </h2>
+            <h2>How many of you are doing unit testing?</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>UNIT TESTING is a level of software testing where individual units/ components of a software are tested. ... A unit is the smallest testable part of any software. It usually has one or a few inputs and usually a single output. In procedural programming, a unit may be an individual program, function, procedure, etc.</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>We can do unit test in 2 ways,</h2>
+            <h2>- By writting php unit test </h2>
+            <h2>- Manually testing which we normally do</h2>
+
+            <p>NOTE: There should be some standard point which we can list down for unit test and for that we can have one document of unit test case,</p>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h1>Lets see some of the common thing we should test,</h1>
+            <h2>- Designing test - checking the web page should look same in some common browsers</h2>
+            <h2>- Spelling or grammatical errors</h2>
+            <h2>- Broken links and images </h2>
+            <h2>- If there is any validation error then the information filled by the user should be there</h2>
+            <h2>- Mandatory field validations with appropriate error messages</h2>
+            <h2>- Sending emails </h2>
+            <h2>- Proper database entries </h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>Unit testing Advantages</h2>
+            <h2>- Improves Quality of Code by Identifies defect that may have come up </h2>
+            <h2>- Finds Software Bugs Early</h2>
+            <h2>- Since the bugs are found early, unit testing helps reduce the cost of bug fixes.</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>Any question about Unit Testing?</h2>
+        </div>
+
+    </div>
+    
+    <div class="section" id="section2">
+        <div class="slide" id="slide2">
+            <h1>Code Editor</h1>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>
+                We are using many different kind of code editors, like, 
+                <br><br>
+                Dreamviewer, Rapid php, Netbeans, sublime Text, VSCode, Phpstorm, Atom and many more available in market...
+                <br><br>
+                but are we really getting most out of it?
+            </h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>Difference between an IDE and editor...</h2>
+        </div>
+        
+        <div class="slide" id="slide2">
+            <h2>The term “IDE” comes from Integrated Development Environment. It is intended as a set of tools that all work together: text editor, compiler, build or make integration, debugging, etc. 
+                
+            <br ><br >... An editor is simply that, a tool that is designed to edit text.</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>VSCode shortcuts and some useful extensions</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>Auto Rename Tag</h2>
+            <h2>Bracket Pair Colorizer</h2>
+            <h2>DotENV</h2>
+            <h2>Indenticator</h2>
+            <h2>PHP Namespace Resolver</h2>
+            <h2>PHP Intelephense</h2>
+            <h2>PHP DocBlocker</h2>
+        </div>
+        <div class="slide" id="slide2">
+            <h2>Php cs fixer</h2>
+            <h2>Prettier - Code formatter</h2>
+            <h2>PHP Constructor</h2>
+            <h2>PHP Getter & Setters</h2>
+            <h2></h2>
+            <h2></h2>
+            <h2></h2>
+            <h2></h2>
+        </div>
+        <div class="slide" id="slide2">
+            <h2>- Shift + Cmd + X open download extension side bar with </h2>
+            <h2>- Shift + Cmd + E to go to tree view</h2>
+            <h2>- Shift + Cmd + F = global search</h2>
+            <h2>- Ctrl + Alt + N to create new file</h2>
+            <h2>- Ctrl + B toggle side bar</h2>
+            <h2>- Ctrl + R changed key binding for "go to symbol to" - got to file and look for symbol </h2>
+            <h2>- Ctrl + Shift + R to go to symbol in whole project</h2>
+            <h2></h2>
+        </div>
+        <div class="slide" id="slide2">
+            <h2>- Ctrl + Shift + \ OR Ctrl + M  backward slash to go to closing bracket </h2>
+            <h2>- Ctrl + Shift + T to reopen last closed tab/file</h2>
+            <h2>- Ctrl + ` to open up a terminal in editor</h2>
+            <h2>- toggle activity bar</h2>
+            <h2>- Alt + Enter to open context menu and use some of its options</h2>
+            <h2>- can change default font family </h2>
+            <h2>- can create snippets	</h2>
+            <h2></h2>
+        </div>
+        <div class="slide" id="slide2">
+            <h2>Multiple Cursor</h2>
+            <h2>- Ctrl + D and keep pressing D to select multiple</h2>
+            <h2>- Ctrl + Shift + G select all similar words together </h2>
+            <h2>- Use Alt and click</h2>
+            <h2>- Ctrl + click to go to definition of class file</h2>
+            <h2>- Ctrl + Shift + D duplicate the line</h2>
+            <h2></h2>
+        </div>
+        <div class="slide" id="slide2">
+            <h2></h2>
+            <h2>- F5, import namespace</h2>
+            <h2>- F6, expand class path</h2>
+            <h2>- F7, insert constructor property</h2>
+        </div>
+    </div>
+
+    <div class="section" id="section2">
+        <div class="slide" id="slide2">
+            <h2>Some coding Guidelines and Tips</h2>
+        </div>
+        <div class="slide" id="slide2">
+            <h1>Refactor code</h1>
+        </div>
+        <div class="slide" id="slide2">
+            <h2>Validations,
+                <br /> Multiple Insert/Update main tables and related tables ,
+                <br /> File uploads,
+                <br /> Api Calls,
+                <br /> Email sending,
+                <br /> and many more things...
+            </h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>What you have to do in refactor is look at your code and see where it can be improved</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>Reusable Service classes or Design patter like observer pattern or Traits or Common functions...</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>Level of indentation - Soft rule no more than 1 - Hard rule Never more than 2</h2>
+            <h2>For this, you can extract the code in protected methods, goal is to keep the method as short as possible</h2>
+            <h2>if-else in loops</h2>
+            <h2>multiple conditions in if</h2>
+            <h2>data_get, data_set, array_wrap, optional, filter, reject</h2>
+            <h2>We should not use else statement, everytime you do there is a better solution</h2>
+            <h2>Can use ternery operator instead of else block! BEST?</h2>
+        </div>
+
+        <div class="slide" id="slide2">
+            <h2>Improved code readability and reduced complexity,</h2>
+            <h2>Improve source-code maintainability,</h2>
+            <h2>More expressive internal architecture,</h2>
+            <h2>Improve extensibility</h2>
+            <h2>Reduce code commenting</h2>
+        </div>
+        
+        
+    </div>
+    
+    <div class="section" id="section2">
+        <div class="slide" id="slide2">
+            <h1>Extra Credits</h1>
+            <h2>runjs console software, <br/>
+                https://runjs.dev/</h2>
+        </div>
+	</div>
+
+	<div class="section" id="section2">
+		<div class="intro">
+			<h1>Thank you for bearing me :) <br />...Any Question?</h1>
+			<h2>Suggestion and Feedback</h2>
+		</div>
+	</div>
+
+</div>
+
+<script type="text/javascript" src="../dist/fullpage.js"></script>
+<script type="text/javascript" src="examples.js"></script>
+
+<script type="text/javascript">
+    var myFullpage = new fullpage('#fullpage');
+</script>
+
+</body>
+</html>
